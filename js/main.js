7ef138e010813204742a5c608e964387a12c4ac7
Evgeniyatorva <evgeniyatorva@mail.ru>
2018-11-09 21:41:31 +0300

    add adaptive
    add adaptive, fix slider, add menu

--- js/main.js
+++ js/main.js
@@ -1,28 +1,55 @@
+// SLIDER REVIEW
 $('.review').bxSlider({
   minSlides: 1,
   maxSlides: 3,
-  slideWidth: 390
+  slideWidth: 390,
+  moveSlides: 1,
+  controls: false
 });
 
+// SLIDER MODEL
 $('.model').bxSlider({
   minSlides: 1,
   maxSlides: 5,
-  slideWidth: 234
+  slideWidth: 234,
+  controls: false
 });
 
-$( function() {
-  var icons = {
-    header: "ui-icon-circle-arrow-e",
-    activeHeader: "ui-icon-circle-arrow-s"
-  };
-  $( "#accordion" ).accordion({
-    icons: icons
-  });
-  $( "#toggle" ).button().on( "click", function() {
-    if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
-      $( "#accordion" ).accordion( "option", "icons", null );
-    } else {
-      $( "#accordion" ).accordion( "option", "icons", icons );
-    }
-  });
-} );
+//ACCORDION 
+$("#accordion").accordion();
+
+// MENU
+var check = document.querySelector('.checkbox-toggle');
+
+var menu = document.querySelector('.menu');
+var menuDiv = document.querySelector('.menu > div');
+var menuDivDiv = document.querySelector('.menu > div > div');
+
+var hamburgerDiv = document.querySelector('.hamburger > div')
+
+check.onclick = function () {
+  menu.classList.toggle('menu-on');
+  menuDiv.classList.toggle('menu-div-on');
+  menuDivDiv.classList.toggle('menu-div-div-on');
+  hamburgerDiv.classList.toggle('hamburgerAnim');
+}
+
+var menuLink = document.querySelectorAll('ul > li > a');
+
+for (i = 0; i < menuLink.length; i++) {
+  menuLink[i].onclick = function (e) {
+    e.preventDefault();
+
+    menu.classList.toggle('menu-on');
+    menuDiv.classList.toggle('menu-div-on');
+    menuDivDiv.classList.toggle('menu-div-div-on');
+    hamburgerDiv.classList.toggle('hamburgerAnim');
+    check.checked = false;
+
+    var selector = this.getAttribute('href');
+    var h = $(selector);
+    $('html, body').animate({
+      scrollTop: h.offset().top - 100
+    }, 0);
+  }
+}
